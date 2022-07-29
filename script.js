dda6d031828268792f4e859a255713915637eb43
Logan Kloft <lkloft@outlook.com>
2022-07-29 15:34:46 -0700

    Finish script for first 6 requirements

--- script.js
+++ script.js
@@ -1,3 +1,126 @@
+// nodes / node lists
+const numberBtns = document.querySelectorAll('.number');
+const opBtns = document.querySelectorAll('.op');
+const equalBtn = document.querySelector('.equal');
+const clearBtn = document.querySelector('.clear');
+const displayNode = document.querySelector('.display-text');
+
+// array of strings
+let operators = ['+', '-', '*', '/'];
+
+// strings
+let displayText = '';
+
+let leftOperand = null;
+let rightOperand = null;
+let op = null;
+
+// flags
+let operatorsEnabled = false;
+
+function display() {
+    displayText = displayText.concat(this.innerText);
+
+    // prevent entering consecutive operators by disabling operator (op) buttons
+    // also disable equals button, since doesn't make sense with only one operand.
+    if (isOperator(this)) {
+        disableOperators();
+        disableEquals();
+    }
+
+    if(isNumber(this)) {
+        enableOperators();
+    }
+
+    expression(this);
+    refreshDisplay();
+}
+
+function expression(node) {
+    if (isOperator(node)) {
+        if (op == null) {
+            op = node.innerText;
+        } else {
+            operate(op, leftOperand, rightOperand);
+            op = node.innerText;
+        }
+    } else {
+        if (leftOperand == null) {
+            leftOperand = node.innerText; 
+        } else if (op == null) {
+            leftOperand = leftOperand.concat(node.innerText);
+        }else if (rightOperand == null) {
+            rightOperand = node.innerText;
+            enableEquals();
+        } else {
+            console.log("Nooo");
+            rightOperand = rightOperand.concat(node.innerText);
+        }
+    }
+}
+
+function isOperator(node) {
+    let str = node.innerText;
+    return operators.includes(str);
+}
+
+function isNumber(node) {
+    return !isNaN(+node.innerText);
+}
+
+function clearDisplay() {
+    displayText = '';
+    leftOperand = null;
+    op = null;
+    rightOperand = null;
+    disableEquals();
+    disableOperators();
+    refreshDisplay();
+}
+
+function refreshDisplay() {
+    displayNode.innerText = displayText;
+}
+
+function enableOperators() {
+    if (!operatorsEnabled) {
+        opBtns.forEach(op => op.addEventListener('click', display));
+        operatorsEnabled = !operatorsEnabled;
+    }
+}
+
+function disableOperators() {
+    if (operatorsEnabled) {
+        opBtns.forEach(op => op.removeEventListener('click', display));
+        operatorsEnabled = !operatorsEnabled;
+    }
+}
+
+function enableClear() {
+    clearBtn.addEventListener('click', clearDisplay);
+}
+
+function enableNumbers() {
+    numberBtns.forEach(number => {
+        number.addEventListener('click', display)
+    });
+}
+
+function enableEquals() {
+    equalBtn.addEventListener('click', equals);
+}
+
+function disableEquals() {
+    equalBtn.removeEventListener('click', equals);
+}
+
+function equals() {
+    operate(op, leftOperand, rightOperand);
+    displayText = leftOperand;
+    refreshDisplay();
+    disableEquals();
+}
+
 function add(a, b) {
     return a + b;
 }
@@ -14,15 +137,33 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(op, a, b) {
-    switch(op) {
+function operate(operator, a, b) {
+    console.log(operator, a, b);
+    if (operator == null || a == null || b == null) return;
+
+    a = +a;
+    b = +b;
+    let result = null;
+
+    switch(operator) {
         case '+':
-            return add(a, b);
+            result = add(a, b);
+            break;
         case '-':
-            return subtract(a, b);
+            result = subtract(a, b);
+            break;
         case '*':
-            return multiply(a, b);
+            result = multiply(a, b);
+            break;
         case '/':
-            return divide(a, b);
+            result = divide(a, b);
+            break;
     }
+
+    leftOperand = result.toString();
+    op = null;
+    rightOperand = null;
 }
+
+enableNumbers();
+enableClear();
